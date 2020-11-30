3d61198ccbc448cc6a07c02880a4d49a87344463
sarthakgaur <75014896+sarthakgaur@users.noreply.github.com>
2020-11-30 13:45:55 +0530

    Part1 exercise 2 completed

--- part1/exercises/01/course-information/src/index.js
+++ part1/exercises/01/course-information/src/index.js
@@ -1,35 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
-  );
-};
+const Header = (props) => (
+  <h1>{props.course}</h1>
+);
 
-const Content = (props) => {
-  return (
-    <>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
-    </>
-  );
-};
+const Content = (props) => (
+  <>
+    <Part part={props.part1} exercise={props.exercises1} />
+    <Part part={props.part2} exercise={props.exercises2} />
+    <Part part={props.part3} exercise={props.exercises3} />
+  </>
+);
 
-const Total = (props) => {
-  return (
-    <p>
-      Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
-    </p>
-  );
-};
+const Part = (props) => (
+  <p>{props.part} {props.exercise}</p>
+);
+
+const Total = (props) => (
+  <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+);
 
 const App = () => {
   const course = 'Half Stack application development';
