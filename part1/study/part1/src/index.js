ff28407c749fad0132451f4425851970eda8fed0
sarthakgaur <75014896+sarthakgaur@users.noreply.github.com>
2020-12-06 23:24:27 +0530

    Completed 6 more study sections of part1

--- part1/study/part1/src/index.js
+++ part1/study/part1/src/index.js
@@ -350,4 +350,184 @@ const Button2 = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 );
 
-ReactDOM.render(<App11 />, document.getElementById('root'));
+// ReactDOM.render(<App11 />, document.getElementById('root'));
+
+/* A more complex state, debugging React apps */
+
+const App12 = () => {
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+
+  return (
+    <div>
+      {left}
+      <button onClick={() => setLeft(left + 1)}>left</button>
+      <button onClick={() => setRight(right + 1)}>right</button>
+      {right}
+    </div>
+  );
+};
+
+// ReactDOM.render(<App12 />, document.getElementById('root'));
+
+const App13 = () => {
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  });
+
+  // const handleLeftClick = () => {
+  //   const newClicks = {
+  //     left: clicks.left + 1,
+  //     right: clicks.right
+  //   };
+  //   setClicks(newClicks);
+  // };
+
+  // const handleRightClick = () => {
+  //   const newClicks = {
+  //     left: clicks.left,
+  //     right: clicks.right + 1
+  //   };
+  //   setClicks(newClicks);
+  // };
+
+  // const handleLeftClick = () => {
+  //   const newClicks = {
+  //     ...clicks,
+  //     left: clicks.left + 1
+  //   };
+  //   setClicks(newClicks);
+  // };
+
+  // const handleRightClick = () => {
+  //   const newClicks = {
+  //     ...clicks,
+  //     right: clicks.right + 1
+  //   };
+  //   setClicks(newClicks);
+  // };
+
+  // It's best not to directly update the state like the following:
+
+  // const handleLeftClick = () => {
+  //   clicks.left++;
+  //   setClicks(clicks);
+  // };
+
+  const handleLeftClick = () => setClicks({ ...clicks, left: clicks.left + 1 });
+
+  const handleRightClick = () => setClicks({ ...clicks, right: clicks.right + 1 });
+
+  return (
+    <div>
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
+    </div>
+  );
+};
+
+// ReactDOM.render(<App13 />, document.getElementById('root'));
+
+/* Handling arrays */
+
+const App14 = () => {
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'));
+    setLeft(left + 1);
+  };
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'));
+    setRight(right + 1);
+  };
+
+  // Don't do this. It's best not to mutate the state directly.
+
+  // const handleLeftClick = () => {
+  //   allClicks.push('L');
+  //   setAll(allClicks);
+  //   setLeft(left + 1);
+  // };
+
+  return (
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
+    </div>
+  );
+};
+
+// ReactDOM.render(<App14 />, document.getElementById('root'));
+
+/* Conditional rendering */
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (<div>the app is used by pressing the buttons</div>);
+  }
+  return (<div>button press history: {props.allClicks.join(' ')}</div>);
+};
+
+const App15 = () => {
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'));
+    setLeft(left + 1);
+  };
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'));
+    setRight(right + 1);
+  };
+
+  return (
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <History allClicks={allClicks} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App15 />, document.getElementById('root'));
+
+/* Rules of Hooks */
+
+const App16 = () => {
+  // These are ok
+  const [age, setAge] = useState(0);
+  const [name, setName] = useState('Juha Tauriainen');
+
+  if (age > 10) {
+    // This does not work!
+    const [foobar, setFoobar] = useState(null);
+  }
+
+  for (let i = 0; i < age; i++) {
+    // Also, this is not good.
+    const [rightWay, setRightWay] = useState(false);
+  }
+
+  const notGood = () => {
+    // and this is also illegal
+    const [x ,setX] = useState(-1000);
+  }
+
+  return (
+    <div></div>
+  );
+};
