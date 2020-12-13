e3b2a1da61a27f423f1656b1782ea77dcd2fb5da
sarthakgaur <75014896+sarthakgaur@users.noreply.github.com>
2020-12-13 21:30:13 +0530

    Post .gitignore changes

--- part0/study/forms_http_post/server_post.js
+++
@@ -1,8 +0,0 @@
-app.post('/new_note', (req, res) => {
-    notes.push({
-        content: req.body.note,
-        date: new Date(),
-    });
-
-    return res.redirect('/notes');
-});
--- part0/study/manipulating_dom/add_note.js
+++
@@ -1,6 +0,0 @@
-let list = document.getElementById('ul')[0];
-
-let newElement = document.createElement('li');
-newElement.textContent = 'Page manipulation from console is easy';
-
-list.appendChild(newElement);
