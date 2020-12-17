3c7db41c10b9f3c0b95affc7c9c07da77b13d312
sarthakgaur <75014896+sarthakgaur@users.noreply.github.com>
2020-12-17 21:34:10 +0530

    Completed 6 more study sections of part2

--- part2/study/part2/src/App.js
+++ part2/study/part2/src/App.js
@@ -1,24 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import Note from './components/Note';
-import axios from 'axios';
+import noteService from './services/notes';
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('');
   const [showAll, setShowAll] = useState(true);
 
+  /* Changing the important of notes */
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(n => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote));
+      })
+      .catch(error => {
+        alert(`The node '${note.content}' was alrady deleted from server`);
+        setNotes(notes.filter(n => n.id !== id));
+      });
+  };
+
   /* Effect-hooks */
 
   useEffect(() => {
-    console.log('effect');
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfulled');
-        setNotes(response.data);
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes);
       });
   }, []);
-  console.log('render', notes.length, 'notes');
 
   /* Filtering Displayed Elements */
 
@@ -35,11 +49,16 @@ const App = () => {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() < 0.5,
-      id: notes.length + 1,
     };
 
-    setNotes(notes.concat(noteObject));
-    setNewNote('');
+    /* Sending Data to the Server */
+
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote));
+        setNewNote('');
+      });
   };
 
   return (
@@ -51,7 +70,12 @@ const App = () => {
         </button>
       </div>
       <ul>
-        {notesToShow.map((note) => <Note key={note.id} note={note} />)}
+        {notesToShow.map((note, i) =>
+          <Note
+            key={i}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />)}
       </ul>
       <form onSubmit={addNote}>
         <input value={newNote} onChange={handleNoteChange} />
