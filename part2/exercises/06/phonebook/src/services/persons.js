361e1f29c8842892a52672e4d2d3c17d212caf08
sarthakgaur <75014896+sarthakgaur@users.noreply.github.com>
2020-12-18 17:17:53 +0530

    Formatting changes

--- part2/exercises/06/phonebook/src/services/persons.js
+++ part2/exercises/06/phonebook/src/services/persons.js
@@ -3,19 +3,19 @@ import axios from 'axios';
 const baseUrl = 'http://localhost:3001/persons';
 
 const getAll = () => {
-    return axios.get(baseUrl).then(response => response.data);
+  return axios.get(baseUrl).then(response => response.data);
 };
 
 const create = (newObject) => {
-    return axios.post(baseUrl, newObject).then(response => response.data);
+  return axios.post(baseUrl, newObject).then(response => response.data);
 };
 
 const remove = (id) => {
-    return axios.delete(`${baseUrl}/${id}`);
+  return axios.delete(`${baseUrl}/${id}`);
 };
 
 const update = (id, person) => {
-    return axios.put(`${baseUrl}/${id}`, person).then(response => response.data);
+  return axios.put(`${baseUrl}/${id}`, person).then(response => response.data);
 };
 
 const exports = { getAll, create, remove, update };
